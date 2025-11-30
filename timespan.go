d7fabcf003462c31df27111c15fef7b2d7a76f9a
Raghvendra Kumar Singh <raghvendra14fb@gmail.com>
2025-11-30 12:57:54 +0530

    feat: add methods for timespan

--- timespan.go
+++ timespan.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -8,4 +9,30 @@ type TimeSpan struct {
 	StartTime time.Time
 	EndTime   time.Time
 	IsRunning bool
+}
+
+func (ts *TimeSpan) IsValid() bool {
+	if ts.StartTime.IsZero() {
+		return false
+	}
+	if ts.EndTime.IsZero() && !ts.IsRunning {
+		return false
+	}
+	if ts.IsRunning && !ts.EndTime.IsZero() {
+		return false
+	}
+	if !ts.EndTime.IsZero() && ts.EndTime.Before(ts.StartTime) {
+		return false
+	}
+	return true
+}
+
+func (ts *TimeSpan) Format() (string, error) {
+	if !ts.IsValid() {
+		return "", fmt.Errorf("invalid TimeSpan: %v", ts)
+	}
+	if ts.IsRunning {
+		return fmt.Sprintf("%s-", ts.StartTime.Format(TimeFormat)), nil
+	}
+	return fmt.Sprintf("%s-%s", ts.StartTime.Format(TimeFormat), ts.EndTime.Format(TimeFormat)), nil
 }
