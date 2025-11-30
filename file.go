330b81309de401926a06406b8f6b903800590573
Raghvendra Kumar Singh <raghvendra14fb@gmail.com>
2025-11-30 13:16:50 +0530

    refactor: move file related functions to new file

---
+++ file.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type FileDetails struct {
+	FileName   string
+	LineNumber int
+}
+
+func (fd *FileDetails) IsValid() bool {
+	return fd.FileName != "" && fd.LineNumber > 0
+}
+
+func readLinesFromFile(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading file: %v", err)
+	}
+
+	return lines, nil
+}
+
+func writeLinesToFile(filename string, lines []string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("error creating file: %v", err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	for _, line := range lines {
+		if _, err := writer.WriteString(line + "\n"); err != nil {
+			return fmt.Errorf("error writing to file: %v", err)
+		}
+	}
+
+	return nil
+}
--- task.go
+++ task.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strings"
 	"time"
 )
-
-type FileDetails struct {
-	FileName   string
-	LineNumber int
-}
-
-func (fd *FileDetails) IsValid() bool {
-	return fd.FileName != "" && fd.LineNumber > 0
-}
 
 type Task struct {
 	Title         string
@@ -310,43 +299,4 @@ func wrapSliceInMTaskComment(value []string) []string {
 	wrappedValue = append(wrappedValue, value...)
 	wrappedValue = append(wrappedValue, "-->")
 	return wrappedValue
-}
-
-func readLinesFromFile(filename string) ([]string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, fmt.Errorf("error opening file: %v", err)
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("error reading file: %v", err)
-	}
-
-	return lines, nil
-}
-
-func writeLinesToFile(filename string, lines []string) error {
-	file, err := os.Create(filename)
-	if err != nil {
-		return fmt.Errorf("error creating file: %v", err)
-	}
-	defer file.Close()
-
-	writer := bufio.NewWriter(file)
-	defer writer.Flush()
-
-	for _, line := range lines {
-		if _, err := writer.WriteString(line + "\n"); err != nil {
-			return fmt.Errorf("error writing to file: %v", err)
-		}
-	}
-
-	return nil
 }
