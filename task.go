7c2b9cdcf51c19494d46f3ffea92b5feccb2613c
Raghvendra Kumar Singh <raghvendra14fb@gmail.com>
2025-11-30 12:54:42 +0530

    refactor: move parsers to different file

--- task.go
+++ task.go
@@ -48,51 +48,6 @@ type Task struct {
 	HasProperties bool
 }
 
-func TryParseSchedule(task *Task, value string) error {
-	val, err := time.ParseInLocation("2006/01/02", value, time.Local)
-	if err != nil {
-		return err
-	}
-	task.Scheduled = val
-	return nil
-}
-
-func TryParseDeadline(task *Task, value string) error {
-	val, err := time.ParseInLocation("2006/01/02", value, time.Local)
-	if err != nil {
-		return err
-	}
-	task.Deadline = val
-	return nil
-}
-
-func TryParseClockData(task *Task, value string) error {
-	for timeSpan := range strings.SplitSeq(value, "\n") {
-		times := strings.Split(timeSpan, " - ")
-		startTime, err := time.ParseInLocation("2006/01/02 15:04", times[0], time.Local)
-		if err != nil {
-			return err
-		}
-		endTime, err := time.ParseInLocation("2006/01/02 15:04", times[1], time.Local)
-		if err != nil {
-			return err
-		}
-		task.ClockData = append(task.ClockData, TimeSpan{StartTime: startTime, EndTime: endTime})
-	}
-	return nil
-}
-
-type TaskPropertyParse struct {
-	Parse    func(task *Task, value string) error
-	Property string
-}
-
-var PropertyParsers = []TaskPropertyParse{
-	{TryParseSchedule, PropertyTypeNames[Scheduled]},
-	{TryParseDeadline, PropertyTypeNames[Deadline]},
-	{TryParseClockData, PropertyTypeNames[ClockData]},
-}
-
 func (task *Task) ParseProperties(propertyMap map[string]string) error {
 	for _, parser := range PropertyParsers {
 		value, ok := propertyMap[parser.Property]
