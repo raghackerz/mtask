cd2199c5a99b96ee938daa7f9c6bfea6701ae9de
Raghvendra Kumar Singh <raghvendra14fb@gmail.com>
2025-12-27 16:06:51 +0530

    refactor: break into reusable functions

--- main.go
+++ main.go
@@ -7,14 +7,51 @@ import (
 	"log"
 )
 
+var TasksInFile map[string][]Task = make(map[string][]Task)
+
 func main() {
-	out, err := GetAllMatchesInRootDir()
+	err := SyncAllTasks()
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(TasksInFile)
+}
+
+func SyncAllTasks() error {
+	out, err := GetAllMatchesInRootDir()
+	if err != nil {
+		return err
+	}
+	tasks, err := ParseMatchesToTasks(out)
+	if err != nil {
+		return err
+	}
+	for _, task := range tasks {
+		if _, ok := TasksInFile[task.FileDetails.FileName]; !ok {
+			TasksInFile[task.FileDetails.FileName] = make([]Task, 0)
+		}
+		TasksInFile[task.FileDetails.FileName] = append(TasksInFile[task.FileDetails.FileName], task)
+	}
+	return nil
+}
+
+func SyncTasksInAFile(filepath string) error {
+	out, err := GetAllMatchesInAFile(filepath)
+	if err != nil {
+		return err
+	}
+	tasks, err := ParseMatchesToTasks(out)
+	if err != nil {
+		return err
+	}
+	TasksInFile[filepath] = tasks
+	return nil
+}
+
+func ParseMatchesToTasks(match []byte) ([]Task, error) {
 	var fileName string
 	tasks := make([]Task, 0)
-	for v := range bytes.SplitSeq(out, []byte("\n")) {
+	for v := range bytes.SplitSeq(match, []byte("\n")) {
 		var r RipgrepResult
 		if len(bytes.TrimSpace(v)) == 0 {
 			continue
@@ -42,8 +79,8 @@ func main() {
 				task.FileDetails.LineNumber = currentLineNumber
 				task.PopulateDetails(match.Match.Text)
 				tasks = append(tasks, task)
-				fmt.Println(task)
 			}
 		}
 	}
+	return tasks, nil
 }
